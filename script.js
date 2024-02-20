ae7cc8c982adacb16b308f99930cd1cb01913c73
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-02-20 23:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        // Leap year: divisible by 4 and not divisible by 100 unless also divisible by 400
+        return 366;
+    } else {
+        // Non-leap year
+        return 365;
+    }
+}
+
+const year = prompt("Enter Year");
+alert(daysOfAYear(year));
